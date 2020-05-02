aed1903d3593cfcf546f01afe4860818ccfd7727
derrickip34 <derrickip34@gmail.com>
2020-05-02 17:47:18 +0300

    add user's profile picture

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template,redirect,url_for,abort
+from flask import render_template,redirect,url_for,abort,request
 from . import main
-from .. import db
+from .. import db,photos
 from .forms import PitchForm,UpdateForm
 from ..models import Pitch,User
 from flask_login import login_required
@@ -52,3 +52,14 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname = user.username))
 
     return render_template('profile/update.html',update=update)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
