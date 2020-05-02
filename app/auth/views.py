4f466b4a618de6d39f3f217c893355fed8af8181
derrickip34 <derrickip34@gmail.com>
2020-05-02 16:04:19 +0300

    create login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,24 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
+from flask_login import login_user,logout_user,login_required
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
+    login = LoginForm()
+    if login.validate_on_submit:
+        user = User.query.filter_by(email = login.email.data).first()
+        if user is not None and user.verify_password(login.password.data):
+            login_user(user,login.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-    return render_template('auth/login.html')
+        flash('Invalid username or password')
+
+    title = 'Login'
+
+    return render_template('auth/login.html',login_form = login)
 
 @auth.route('/register',methods=['GET','POST'])
 def register():
@@ -21,3 +32,9 @@ def register():
         title = 'New Account'
 
     return render_template('auth/register.html', registration = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
