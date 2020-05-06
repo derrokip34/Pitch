e67be4ef97ee365ee2be4243c3a123b0a0608a89
derrickip34 <derrickip34@gmail.com>
2020-05-06 17:40:28 +0300

    change start.sh file

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME=derrickip34@gmail.com
 export MAIL_PASSWORD=montolivo
 export SECRET_KEY=derroki1998
+export SQLALCHEMY_DATABASE_URI=sqlite:///site.db
 
 python3.6 manage.py server
