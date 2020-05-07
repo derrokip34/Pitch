bda5d875095840e890b3493620a98b050655948c
derrickip34 <derrickip34@gmail.com>
2020-05-07 11:13:36 +0300

    change mail password

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME=derrickip34@gmail.com
-export MAIL_PASSWORD=montolivo
+export MAIL_PASSWORD=Enkay2008
 export SECRET_KEY=derroki1998
 export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
 
